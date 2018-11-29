6dfbaed31fc1928fe61cf6f051451d7e366a43e4
Patrice CAVEZZAN <pcavezzan@gmail.com>
2018-11-29 18:20:07 +0100

    readPomVersion should read parent version section when version is absent

--- src/readPomVersion.js
+++ src/readPomVersion.js
@@ -8,5 +8,13 @@ module.exports = (file) => {
     if (versionIndex >= 0) {
         return obj.root.children[versionIndex].content;
     }
+
+    // If version does not exists in a child maven module, it means version comes from parent.
+    const parentIndex = obj.root.children.findIndex(child => child.name === 'parent');
+    if (parentIndex >= 0) {
+        const parentVersionIndex = obj.root.children[parentIndex].children.findIndex(child => child.name === 'version');
+        return obj.root.children[parentIndex].children[parentVersionIndex].content;
+    }
+    
     throw new Error(`Could not find version in pom file ${file}`);
 };
--- test/readPomVersion.test.js
+++ test/readPomVersion.test.js
@@ -5,18 +5,32 @@ const path = require('path');
 const {expect} = require('chai');
 
 const VALID_POM = '<project xmlns="http://maven.apache.org/POM/4.0.0"><version>0.0.1-SNAPSHOT</version></project>';
+const VALID_POM_CHILD = '<project xmlns="http://maven.apache.org/POM/4.0.0"><parent><version>0.0.2-SNAPSHOT</version></parent></project>';
+const VALIDE_POM_CHILD_OVERRIDE = '<project xmlns="http://maven.apache.org/POM/4.0.0"><parent><version>0.0.2-SNAPSHOT</version></parent><version>0.0.3-SNAPSHOT</version></project>';
 const INVALID_POM = '<project xmlns="http://maven.apache.org/POM/4.0.0"></project>';
 const FILE = path.join(__dirname, 'pom.xml');
+const FILE_MODULE_CHILD = path.join(__dirname, 'pom-child.xml');
+const FILE_MODULE_OVERRIDE = path.join(__dirname, 'pom-child-override.xml')
 
 describe('readPomVersion', () => {
     before(() => {
         fs.writeFileSync(FILE, VALID_POM);
+        fs.writeFileSync(FILE_MODULE_CHILD, VALID_POM_CHILD);
+        fs.writeFileSync(FILE_MODULE_OVERRIDE, VALIDE_POM_CHILD_OVERRIDE);
     });
     after(() => {
         fs.unlinkSync(FILE);
+        fs.unlinkSync(FILE_MODULE_CHILD);
+        fs.unlinkSync(FILE_MODULE_OVERRIDE);
     });
     it('should read version from pom', () => {
         expect(readPomVersion(FILE)).to.equal('0.0.1-SNAPSHOT');
+    });
+    it('should read parent version from pom', () => {
+        expect(readPomVersion(FILE_MODULE_CHILD)).to.equal('0.0.2-SNAPSHOT');
+    });
+    it('should read version override from pom', () => {
+        expect(readPomVersion(FILE_MODULE_OVERRIDE)).to.equal('0.0.3-SNAPSHOT');
     });
     it('should throw an error if pom doesn\'t exist', () => {
         expect(readPomVersion.bind(this, 'non-existing-pom.xml')).to.throw();

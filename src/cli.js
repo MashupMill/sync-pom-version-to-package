2d84e99591fb8228fe97c272b5def87b720f3b84
Aicke Schulz <aicke.schulz@gmail.com>
2021-12-21 09:06:49 +0100

    Replace deprecated functions and values of ArgumentParser
    Related to: https://github.com/MashupMill/sync-pom-version-to-package/issues/34

--- src/cli.js
+++ src/cli.js
@@ -1,13 +1,13 @@
 #!/usr/bin/env node
 const syncPomToPackage = require('./syncPomToPackage');
 const parser = new (require('argparse').ArgumentParser)({
-    addHelp: true,
+    add_help: true,
     description: 'Sync pom.xml version to the package.json.\nUsage: $0'
 });
-parser.addArgument(['--prerelease-suffix'], { defaultValue: '', help: 'Suffix string to apply to the version if it is a prerelease version' });
-parser.addArgument(['--pom-file'], { help: 'Location of the pom.xml file. Defaults to use ./pom.xml' });
-parser.addArgument(['--package-file'], { help: 'Location of the package.json file. Defaults to use ./package.json' });
-parser.addArgument(['--use-yarn'], { action: 'storeTrue', help: 'Use Yarn instead of NPM to update the version' });
-const args = parser.parseArgs();
+parser.add_argument('--prerelease-suffix', { default: '', help: 'Suffix string to apply to the version if it is a prerelease version' });
+parser.add_argument('--pom-file', { help: 'Location of the pom.xml file. Defaults to use ./pom.xml' });
+parser.add_argument('--package-file', { help: 'Location of the package.json file. Defaults to use ./package.json' });
+parser.add_argument('--use-yarn', { action: 'store_true', help: 'Use Yarn instead of NPM to update the version' });
+const args = parser.parse_args();
 
 syncPomToPackage(args.pom_file || undefined, args.package_file || undefined, { prereleaseSuffix: args.prerelease_suffix, useYarn: args.use_yarn });
